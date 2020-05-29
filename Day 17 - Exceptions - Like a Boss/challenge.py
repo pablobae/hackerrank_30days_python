85b17dcef56f154b3e4c56687b844361212f5a8f
Pablo Baenas <contacto@pablobaenas.com>
2020-05-29 10:15:43 +0200

    Day 17 - Exceptions like a Boss

---
+++ Day 17 - Exceptions - Like a Boss/challenge.py
@@ -0,0 +1,19 @@
+
+class Calculator():
+    def power(self, base, exponent):
+        if base >= 0 and exponent >= 0:
+            return base**exponent
+        else:
+            raise ValueError("n and p should be non-negative")
+
+
+
+myCalculator=Calculator()
+T=int(input())
+for i in range(T):
+    n,p = map(int, input().split())
+    try:
+        ans=myCalculator.power(n,p)
+        print(ans)
+    except Exception as e:
+        print(e)   
